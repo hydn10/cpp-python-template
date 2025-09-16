134e1f091abbf34e69faa2ee41e4af051d227c9b
Hydn <marti.llinas.comas@gmail.com>
2025-09-16 15:52:45 +0200

    Fix clang-tidy error

--- examples/cpp_example.cpp
+++ examples/cpp_example.cpp
@@ -3,7 +3,7 @@
 #include "mylib/mylib.hpp"
 
 int main() {
-  double x = 3.0;
+  double const x = 3.0;
   std::cout << "compute_value(" << x << ") = " << mylib::compute_value(x) << "\n";
   return 0;
 }
